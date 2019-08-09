688543b0c5cfbb7fede3a3428e8ec1892b901869
KIMSANGHYUN <ksh9891@gmail.com>
2019-08-09 16:36:57 +0900

    member 메일 작업 보류

---
+++ src/main/java/com/artfactory/project01/todayart/service/EmailService.java
@@ -0,0 +1,25 @@
+package com.artfactory.project01.todayart.service;
+
+import com.artfactory.project01.todayart.model.Mail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public void sendSimpleMessage(final Mail mail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject(mail.getSubject());
+        message.setText(mail.getContent());
+        message.setTo(mail.getTo());
+        message.setFrom(mail.getFrom());
+
+        JavaMailSender emailSender = new JavaMailSenderImpl();
+        emailSender.send(message);
+    }
+
+}
