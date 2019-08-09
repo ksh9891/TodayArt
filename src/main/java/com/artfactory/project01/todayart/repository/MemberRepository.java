89b36ecfe748a26c601f1c02f59826fa75fc255e
KIMSANGHYUN <ksh9891@gmail.com>
2019-08-09 17:27:33 +0900

    회원관리 시작

--- src/main/java/com/artfactory/project01/todayart/repository/MemberRepository.java
+++ src/main/java/com/artfactory/project01/todayart/repository/MemberRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-
     Member findByEmail(String email);
 }
