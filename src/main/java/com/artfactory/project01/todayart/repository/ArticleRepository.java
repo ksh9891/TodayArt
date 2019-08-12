7752245c00b6040356bea1faf4ac38f2023d8a53
jh007v <jh007v@gmail.com>
2019-08-12 12:27:08 +0900

    create 게시물 생성 read 게시물 읽기-view수 카운트 update 업데이트 시 전의 데이터를 덱스트 에디터에 뿌려줘야함 delete 게시판에서의 삭제와 데이터서의 삭제 나누기
    DB업데이트

--- src/main/java/com/artfactory/project01/todayart/repository/ArticleRepository.java
+++ src/main/java/com/artfactory/project01/todayart/repository/ArticleRepository.java
@@ -4,7 +4,6 @@ import com.artfactory.project01.todayart.entity.ArticleVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<ArticleVO,Integer> {
