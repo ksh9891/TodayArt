02a69a7c44c5dd0b4356c21b9fb4ba9f98d3ee34
BChaekyung <ckqkd1002@gmail.com>
2019-08-12 18:19:29 +0900

    onetomany로 변경중

--- src/main/java/com/artfactory/project01/todayart/entity/ProductCategory.java
+++ src/main/java/com/artfactory/project01/todayart/entity/ProductCategory.java
@@ -4,10 +4,8 @@ package com.artfactory.project01.todayart.entity;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -17,18 +15,19 @@ public class ProductCategory {
 
 
     @Id
-    @Column(columnDefinition = "int", name = "category_id", updatable = false, nullable = false)
-    private Integer category_id;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private List<Product> productList;
 
     @Column(name = "category_name")
     private String category_name;
 
-    public Integer getCategory_id() {
-        return category_id;
+    public List<Product> getProductList() {
+        return productList;
     }
 
-    public void setCategory_id(Integer category_id) {
-        this.category_id = category_id;
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
     }
 
     public String getCategory_name() {
