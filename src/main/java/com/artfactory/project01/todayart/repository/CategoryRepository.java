15a5cbf348d0e124d4589c1e2c022110e0372073
ChaekyungB <ckqkd1002@gmail.com>
2019-08-15 17:21:06 +0900

    카트/위시리스트 중복체크 추가

--- src/main/java/com/artfactory/project01/todayart/repository/CategoryRepository.java
+++ src/main/java/com/artfactory/project01/todayart/repository/CategoryRepository.java
@@ -1,8 +1,14 @@
 package com.artfactory.project01.todayart.repository;
 
 
+import com.artfactory.project01.todayart.entity.Cart;
+import com.artfactory.project01.todayart.entity.WishList;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository {
+
+
+
 }
